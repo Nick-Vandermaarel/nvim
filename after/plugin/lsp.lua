38046baaddee69baf273dc15d92253d187223384
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-01-23 14:45:27 -0500

    mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'eslint', 'html', 'scss', 'rust_analyzer'},
+  ensure_installed = {'tsserver', 'eslint', 'html', 'rust_analyzer'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "c_sharp", "vue", "python", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "javascript", "typescript", "c_sharp", "vue", "python", "c", "lua", "vim", "vimdoc", "query", "html", "http" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -18,4 +18,4 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-}
+  }
