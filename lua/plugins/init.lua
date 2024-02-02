36192836dc8c26011933ded337df61beb03740c8
Nick Vandermaarel <nicholasvdm@hotmail.com>
2024-02-02 10:24:47 -0500

    Fix which ke

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,19 +44,19 @@ return {
       }
   },
 
-  {
-    "folke/which-key.nvim",
-    event ="VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-    end
-  },
+    {
+        "folke/which-key.nvim",
+        event ="VeryLazy",
+        init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+            end,
+        opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        }
+    },
 
     "nvim-tree/nvim-tree.lua",
     "nvim-tree/nvim-web-devicons",
