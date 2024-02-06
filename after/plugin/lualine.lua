31af6a9800f17122a0285126b5b73fe843c2e843
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-02-06 09:27:54 -0500

    Git blame and lualine config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,11 +1,13 @@
 local theme = require "lualine.themes.auto"
 local git_blame = require "gitblame"
 vim.g.gitblame_display_virtual_text = 0 -- Remove virtual text from the buffer.
+vim.g.gitblame_message_when_not_committed = "Not commited"
 
 require("lualine").setup {
     options = { theme = theme },
     sections = {
         -- Show the bit blame in lua line.
-        lualine_c = {{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }}
+        lualine_c = {{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }},
+        lualine_x = { "filename", "filetype" },
     }
 }
--- lua/nvdm/set.lua
+++ lua/nvdm/set.lua
@@ -23,7 +23,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 
-vim.o.shell = "pwsh"
+--vim.o.shell = "pwsh"
 vim.opt.title = true
 vim.opt.titlestring = [[%t - %{fnamemodify(getcwd(), ':t')}]]
 
