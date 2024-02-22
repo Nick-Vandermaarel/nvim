717f22d355699a8bb5edae53aaea53abc017d05d
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-02-22 10:15:10 -0500

    reverting color themes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,5 @@
-require("catppuccin").setup({
-    flavor = "mocha",
-    --    transparent_background = true,
-})
-
 function ColorMyPencils(color)
-    color = color or "catppuccin"
+    color = color or "kanagawa"
     vim.cmd.colorscheme(color)
 
     --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,13 +8,7 @@ return {
         dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
     "nvim-telescope/telescope-project.nvim",
-    -- "rebelot/kanagawa.nvim",
-    -- Theme
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000
-    },
+    "rebelot/kanagawa.nvim", -- Theme
     {
         'numToStr/Comment.nvim',
         lazy = false,
