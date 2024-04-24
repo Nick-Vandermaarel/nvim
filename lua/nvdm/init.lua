c11e7b07f73bd2e9f942de732fe34f25a690adaf
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-04-24 09:37:59 -0400

    Package updates and autocmd refactoring

--- lua/nvdm/init.lua
+++ lua/nvdm/init.lua
@@ -35,50 +35,9 @@ vim.g.loaded_netrwPlugin = 1
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
-
 local harpoon = require("harpoon")
 harpoon:setup();
 
--- LSP Attach AutoCMD
-vim.api.nvim_create_autocmd('LspAttach', {
-    desc = "LSP actions",
-    callback = function(event)
-        local nmap = function(keys, func, desc)
-            if desc then
-                desc = "LSP: " .. desc
-            end
-
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc, remap = false })
-        end
-
-        local builtin = require("telescope.builtin")
-        nmap("gd", builtin.lsp_definitions, "[G]oto [D]efinitions")
-        nmap("gr", builtin.lsp_references, "[G]oto [R]eferences")
-        nmap("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
-        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-        nmap("sh", vim.lsp.buf.signature_help, "[S]ignature [H]elp")
-        nmap("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-        nmap("[d", function() vim.diagnostic.goto_next() end)
-        nmap("]d", function() vim.diagnostic.goto_prev() end)
-        nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
-    end
-})
-
--- Auto format AutoCMD
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        require("conform").format({
-            bufnr = args.buf,
-            async = false,
-            timeout_ms = 5000,
-            lsp_fallback = true,
-        })
-    end,
-})
-
-
+require("nvdm.autocmd")
 require("nvdm.remap")
 require("nvdm.set")
