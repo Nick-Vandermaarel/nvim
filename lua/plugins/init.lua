2e8307bdbe20bbefc4c118a00869df07e62dc66f
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-06-04 16:04:29 -0400

    remove toggle term

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,8 +56,5 @@ return {
         dependencies = "nvim-lua/plenary.nvim",
         event = "VeryLazy",
         opts = {}
-    },
-    {
-        'akinsho/toggleterm.nvim', version = "*", config = true
     }
 }
