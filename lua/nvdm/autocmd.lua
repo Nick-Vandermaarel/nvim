4eb5c2a47d549b02ca536173cdf09d8a149f17fd
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-06-20 11:46:27 -0400

    Remove csharp usings on format

--- lua/nvdm/autocmd.lua
+++ lua/nvdm/autocmd.lua
@@ -34,6 +34,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
     callback = function(args)
+        -- Remove usings from C#.
+        if vim.bo[0].filetype == "cs" then
+            vim.cmd("CSFixUsings")
+        end
+
         require("conform").format({
             bufnr = args.buf,
             async = false,
