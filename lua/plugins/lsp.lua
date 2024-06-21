0195b4f695141bab153ac73bac5c175fc9913a3a
Nick Vandermaarel <nicholasvdm@hotmail.com>
2024-06-21 11:10:21 -0400

    Cmp ghost text and tab selection fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -115,13 +115,14 @@ return {
             event = "InsertEnter",
             dependencies = {
                 "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-buffer",           -- source for text in buffer
-                "hrsh7th/cmp-path",             -- source for file system paths
-                "L3MON4D3/LuaSnip",             -- snippet engine
-                "saadparwaiz1/cmp_luasnip",     -- for autocompletion
-                "rafamadriz/friendly-snippets", -- useful snippets
-                "onsails/lspkind.nvim",         -- vs-code like pictograms
-                {                               -- Github Copilot suggestions
+                "hrsh7th/cmp-buffer",                  -- source for text in buffer
+                "hrsh7th/cmp-path",                    -- source for file system paths
+                "hrsh7th/cmp-nvim-lsp-signature-help", -- source for displaying function signatures
+                "L3MON4D3/LuaSnip",                    -- snippet engine
+                "saadparwaiz1/cmp_luasnip",            -- for autocompletion
+                "rafamadriz/friendly-snippets",        -- useful snippets
+                "onsails/lspkind.nvim",                -- vs-code like pictograms
+                {                                      -- Github Copilot suggestions
                     "zbirenbaum/copilot-cmp",
                     config = function()
                         require("copilot_cmp").setup()
@@ -139,25 +140,27 @@ return {
                 local cmp = require('cmp')
                 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-                local luasnip = require("luasnip")
                 local lspkind = require("lspkind")
 
+                local luasnip = require("luasnip")
                 require("luasnip.loaders.from_vscode").lazy_load()
+
+                -- Tab completion fix
+                local has_words_before = function()
+                    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+                    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") ==
+                        nil
+                end
 
                 cmp.setup({
                     sources = cmp.config.sources({
+                        { name = "nvim_lsp_signature_help" },
                         { name = "copilot" },
                         { name = 'nvim_lsp' },
                         { name = "luasnip" },
                         { name = "path" },
                     }),
-                    formatting = {
-                        format = lspkind.cmp_format({
-                            with_text = true,
-                            maxwidth = 50,
-                            ellipsis_char = "..."
-                        }),
-                    },
                     window = {
                         documentation = cmp.config.window.bordered(),
                         completion = cmp.config.window.bordered(),
@@ -167,14 +170,34 @@ return {
                             luasnip.lsp_expand(args.body)
                         end,
                     },
+                    formatting = {
+                        format = lspkind.cmp_format({
+                            mode = "symbol",
+                            max_width = 50,
+                            symbol_map = { Copilot = "" },
+                        }),
+                    },
+                    experimental = {
+                        ghost_text = true,
+                    },
                     mapping = cmp.mapping.preset.insert({
                         -- Select the next item
                         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                         -- select the previous item
                         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 
+                        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
                         -- Accept the completion. Default is <c-y>
                         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                        ['<tab>'] = vim.schedule_wrap(function(fallback)
+                            if cmp.visible() and has_words_before() then
+                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                fallback()
+                            end
+                        end),
 
                         -- Manually trigger a completion from nvim-cmp
                         ['<C-Space>'] = cmp.mapping.complete(),
