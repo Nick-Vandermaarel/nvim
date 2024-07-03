32de4703973efd7f28d9de469d27d98520e2408b
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-07-03 16:29:54 -0400

    Hackly roslyn fix

--- lua/nvdm/autocmd.lua
+++ lua/nvdm/autocmd.lua
@@ -43,4 +43,20 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end,
 })
 
+-- Hack for roslyn lsp, to refresh diagnostics on insert leave
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    pattern = "*",
+    callback = function()
+        local clients = vim.lsp.get_clients({ name = "roslyn" })
+        if not clients or #clients == 0 then
+            return
+        end
+
+        local buffers = vim.lsp.get_buffers_by_client_id(clients[1].id)
+        for _, buf in ipairs(buffers) do
+            vim.lsp.util._refresh("textDocument/diagnostic", { bufnr = buf })
+        end
+    end,
+})
+
 return onAttach;
