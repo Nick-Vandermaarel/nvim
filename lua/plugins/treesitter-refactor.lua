ad70f16e6e5a73b041364e3e01ecdaa51ae74629
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-08-27 14:22:27 -0400

    Adding treesitter refactor

---
+++ lua/plugins/treesitter-refactor.lua
@@ -0,0 +1,23 @@
+return {
+    "nvim-treesitter/nvim-treesitter-refactor",
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            refactor = {
+                highlight_definitions = {
+                    enable = true,
+                    clear_on_cursor_move = true,
+                },
+                navigation = {
+                    enable = true,
+                    keymaps = {
+                        goto_definition = "gnd",
+                        list_definitions = "gnD",
+                        list_definitions_toc = "gO",
+                        goto_next_usage = "<a-*>",
+                        goto_previous_usage = "<a-#>",
+                    }
+                }
+            }
+        }
+    end
+}
