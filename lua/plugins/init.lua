7e95782545108aa7cb3d99408e45bb15e4113a81
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-09-05 13:36:06 -0400

    Re-add nivm tree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -73,4 +73,23 @@ return {
         event = "VeryLazy",
         opts = {}
     },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+        event = "VeryLazy",
+        config = function()
+            require("nvim-tree").setup({
+                sort = {
+                    sorter = "case_sensitive",
+                },
+                view = {
+                    adaptive_size = true,
+                    relativenumber = true
+                },
+            })
+
+            vim.keymap.set("n", "<leader>]", ":NvimTreeToggle<CR>")
+            vim.keymap.set("n", "<leader>'", ":NvimTreeFocus<CR>")
+        end,
+    }
 }
