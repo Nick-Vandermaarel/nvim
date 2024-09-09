c94863fd7389f350c4f4dfadce0161318bef2d1c
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-09-09 15:06:29 -0400

    Nvim tree impl, find file cmd

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,20 @@
+return
+{
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    event = "VeryLazy",
+    config = function()
+        require("nvim-tree").setup({
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                adaptive_size = true,
+                relativenumber = true
+            },
+        })
+
+        vim.keymap.set("n", "<leader>]", ":NvimTreeToggle<CR>")
+        vim.keymap.set("n", "<leader>'", ":NvimTreeFindFile<CR>")
+    end,
+}
