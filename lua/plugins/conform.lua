2ba8ae766403f8dc97f002c3cbcd09395e10e007
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-09-24 16:37:03 -0400

    Switch from prettier to biome

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,14 +5,14 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-                javascript = { "prettierd" },
-                typescript = { "prettierd" },
-                vue = { "prettierd" },
-                scss = { "prettierd" },
-                json = { "prettierd" },
-                html = { "prettierd" },
-                css = { "prettierd" },
-                markdown = { "prettierd" },
+                javascript = { "biome" },
+                typescript = { "biome" },
+                vue = { "biome" },
+                scss = { "biome" },
+                json = { "biome" },
+                html = { "biome" },
+                css = { "biome" },
+                markdown = { "biome" },
                 -- cs = { { "csharpier" } },
             },
             format_on_save = {
