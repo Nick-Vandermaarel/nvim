6d4a131351dcef3a5deeb042a7ffcdbf2f056b1d
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-10-08 09:36:34 -0400

    Refactored cmp

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,58 @@
+return {
+    "yioneko/nvim-cmp",
+    branch = "perf",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",                  -- source for text in buffer
+        "hrsh7th/cmp-path",                    -- source for file system paths
+        "hrsh7th/cmp-nvim-lsp-signature-help", -- source for displaying function signatures
+        "onsails/lspkind.nvim",                -- vs-code like pictograms
+    },
+    opts = function(_, opts)
+        opts.sources = opts.sources or {}
+        table.insert(opts.sources, {
+            name = "lazydev",
+            group_index = 0, -- set group index to 0 to skil loading LuaLs completions
+        })
+
+        return opts;
+    end,
+    config = function()
+        local cmp = require('cmp')
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            sources = cmp.config.sources({
+                { name = "nvim_lsp_signature_help" },
+                { name = 'nvim_lsp' },
+                { name = "path" },
+                { name = "buffer" }
+            }),
+            window = {
+                documentation = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered(),
+            },
+            experimental = {
+                ghost_text = { hl_group = "CmpGhostText" },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<tab>'] = cmp.mapping.confirm({ select = true }),
+
+                -- Manually trigger a completion from nvim-cmp
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
+        })
+
+        -- Add parentheses after selecting function or method item.
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        cmp.event:on(
+            "confirm_done",
+            cmp_autopairs.on_confirm_done()
+        )
+    end
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,15 +40,6 @@ return {
                 })
             end
 
-            local function organizeImports()
-                local params = {
-                    command = "_typescript.organizeImports",
-                    arguments = { vim.api.nvim_buf_get_name(0) },
-                    title = ""
-                }
-                vim.lsp.buf.execute_command(params)
-            end
-
             vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
                 vim.lsp.handlers.hover,
                 { border = "rounded" }
@@ -83,113 +74,10 @@ return {
                                 }
                             },
                             filetypes = { "typescript", "javascript", "vue" },
-                            commands = {
-                                OrganizeImports = {
-                                    organizeImports,
-                                    description = "Organize Imports"
-                                }
-                            },
                         })
                     end,
                 }
             })
         end,
-    },
-    {
-        {
-            "yioneko/nvim-cmp",
-            branch = "perf",
-            event = "InsertEnter",
-            dependencies = {
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-buffer",                  -- source for text in buffer
-                "hrsh7th/cmp-path",                    -- source for file system paths
-                "hrsh7th/cmp-nvim-lsp-signature-help", -- source for displaying function signatures
-                "onsails/lspkind.nvim",                -- vs-code like pictograms
-                -- {                                      -- Github Copilot suggestions
-                --     "zbirenbaum/copilot-cmp",
-                --     config = function()
-                --         require("copilot_cmp").setup()
-                --     end
-                -- },
-            },
-            opts = function(_, opts)
-                opts.sources = opts.sources or {}
-                table.insert(opts.sources, {
-                    name = "lazydev",
-                    group_index = 0, -- set group index to 0 to skil loading LuaLs completions
-                })
-
-                opts.performance = {
-                    debounce = 0, -- default is 60ms
-                    throttle = 0, -- default is 30ms
-                }
-                return opts;
-            end,
-            config = function()
-                local cmp = require('cmp')
-                local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-                local lspkind = require("lspkind")
-
-                cmp.setup({
-                    sources = cmp.config.sources({
-                        { name = "nvim_lsp_signature_help" },
-                        -- { name = "copilot" },
-                        { name = 'nvim_lsp' },
-                        { name = "path" },
-                    }),
-                    window = {
-                        documentation = cmp.config.window.bordered(),
-                        completion = cmp.config.window.bordered(),
-                    },
-                    formatting = {
-                        format = lspkind.cmp_format({
-                            mode = "symbol",
-                            max_width = 50,
-                            symbol_map = { Copilot = "" }
-                        }),
-                    },
-                    experimental = {
-                        ghost_text = { hl_group = "CmpGhostText" },
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        -- Select the next item
-                        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                        -- select the previous item
-                        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-
-                        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-                        -- Accept the completion. Default is <c-y>
-                        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                        ['<tab>'] = cmp.mapping.confirm({ select = true }),
-
-                        -- Manually trigger a completion from nvim-cmp
-                        ['<C-Space>'] = cmp.mapping.complete(),
-                    }),
-                })
-
-                -- Add parentheses after selecting function or method item.
-                local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-                cmp.event:on(
-                    "confirm_done",
-                    cmp_autopairs.on_confirm_done()
-                )
-            end
-        },
-        -- {
-        --     "zbirenbaum/copilot.lua",
-        --     cmd = "Copilot",
-        --     event = "InsertEnter",
-        --     fix_pairs = true,
-        --     config = function()
-        --         require("copilot").setup({
-        --             suggestion = { enabled = false },
-        --             panel = { enabled = false }
-        --         })
-        --     end
-        -- },
     }
 }
