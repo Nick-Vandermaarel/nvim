48afb9945792a7e1471b1ba184c913328c1e15b7
Nick Vandermaarel <nicholasv@bosinnovations.com>
2024-11-07 09:29:32 -0500

    Telesctop dynamics workspace commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,8 +17,7 @@ return {
         vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
         vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Telescope live grep" })
-        -- vim.keymap.set('n', '<leader>ps', function()
-        --     builtin.grep_string({ search = vim.fn.input("Grep > ") });
-        -- end)
+        vim.keymap.set("n", "<leader>pt", builtin.lsp_dynamic_workspace_symbols,
+            { desc = "Telescope Dynamic Workspace Symbols" })
     end,
 }
