eb8f13e4897e0f80a6c53e79bc36f89df826a201
Nick Vandermaarel <nicholasv@bosinnovations.com>
2025-01-16 09:24:02 -0500

    Spelling

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,8 +8,8 @@ return {
     config = function()
         require("gitsigns").setup()
 
-        vim.keymap.set("n", "<leader>gp", ":Gitsignts preview_hunk<CR>", { desc = "Git Preview" });
-        vim.keymap.set("n", "<leader>gt", ":Gitsignts toggle_current_line_blame<CR>",
+        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Git Preview" });
+        vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>",
             { desc = "Git Toggle Current Line Blame" });
     end
 }
