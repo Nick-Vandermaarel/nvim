ea9123d7346ddc22015eec8fd5ef4d7beb0919b6
Nick Vandermaarel <nicholasv@bosinnovations.com>
2025-03-04 10:09:18 -0500

    changes

--- lua/nvdm/set.lua
+++ lua/nvdm/set.lua
@@ -39,7 +39,17 @@ vim.opt.list = true
 vim.opt.signcolumn = "yes"
 
 vim.diagnostic.config({
-    float = { border = "rounded" }
+    float = { border = "rounded" },
+    virtual_text = true,   -- Show diagnostics inline
+    signs = true,          -- Show signs in the sign column
+    underline = true,      -- Underline text with issues
+    update_in_insert = false,  -- Don't update diagnostics in insert mode
+    severity_sort = true,  -- Sort diagnostics by severity
+    virtual_lines = false, -- Don't show virtual lines
+    message = {            -- Configure message shown in command line
+        show = true,       -- Show message in command line
+        prefix = "●"       -- Prefix for messages
+    }
 })
 
 if vim.fn.has('win32') == 1 then
