e6151b480e1a436a1349dd6c96cb5e9738fd53f2
Nick Vandermaarel <nicholasv@bosinnovations.com>
2025-03-26 09:47:38 -0400

    Easy dotnet conf

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,5 +1,4 @@
-return
-{
+return {
     "GustavEikaas/easy-dotnet.nvim",
     dependencies = { "nvim-lua/plenary.nvim", 'folke/snacks.nvim', },
     event = "VeryLazy",
@@ -7,8 +6,13 @@ return
         require("easy-dotnet").setup({
             test_runner = {
                 viewmode = "split",
-                enable_buffer_test_execution = true,
+                enable_buffer_test_execution = true, // experimental
             },
+            auto_bootstrap_namespace = {
+                type = "file_scoped",
+                enabled = true,
+            },
+            picker = "snacks",
         })
     end
 }
