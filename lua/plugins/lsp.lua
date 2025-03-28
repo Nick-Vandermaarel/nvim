c76247efd2931d3d24335574ac6b5acae4854036
Nick Vandermaarel <nicholasvdm@hotmail.com>
2025-03-28 11:46:01 -0400

    C# update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,7 +17,18 @@ return {
                     settings = {
                         ['csharp|code_lens'] = {
                             dotnet_enable_references_code_lens = true,
-                        }
+                        },
+                        ["csharp|completion"] = {
+                            dotnet_show_completion_items_from_unimported_namespaces = true,
+                        },
+                        ["csharp|inlay_hints"] = {
+                            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                            csharp_enable_inlay_hints_for_types = true,
+                        },
+                        ["csharp|formatting"] = {
+                            dotnet_organize_imports_on_format = true,
+                        },
                     },
                     on_attach = function(client, bufnr)
                         lspUtils.onAttach({ client = client, bufnr = bufnr });
