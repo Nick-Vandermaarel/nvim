5e1d94f1a11850adf437e92ed15d40ccb82250d0
Nick Vandermaarel <nicholasvdm105@gmail.com>
2025-06-20 15:29:21 -0400

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,15 +17,13 @@ return {
                     "snacks_dashboard",
                     "lazy",
                     "mason",
-                }
+                },
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
             },
             sections = {
                 lualine_b = {
-                    "branch", "diff",
-                    {
-                        "diagnostics",
-                        update_in_insert = true
-                    },
+                    "branch", "diff", "diagnostics",
                 },
                 lualine_c = {
                     "filename",
