939f1d2981c8b5df8dc7215ae90d112d5b7ea39c
Nick Vandermaarel <nicholasvdm105@gmail.com>
2025-08-01 08:56:14 -0400

    fix highlighting

--- lua/nvdm/init.lua
+++ lua/nvdm/init.lua
@@ -56,19 +56,23 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'BufReadPost' }, {
 vim.opt.winbar = '%#MyFileIcon#%{v:lua.get_file_icon()} %#WinBar#%t%r%#MyModified#%{&modified ? " ●" : " "}'
 vim.api.nvim_set_hl(0, 'MyModified', { fg = '#ff9800' })
 
--- todo, hack, note, comment plugin
-vim.api.nvim_set_hl(0, 'TodoComment', { bg = '#50fa7b', fg = '#000000', bold = true })
-vim.api.nvim_set_hl(0, 'HackComment', { bg = '#ffb86c', fg = '#000000', bold = true })
-vim.api.nvim_set_hl(0, 'NoteComment', { bg = '#8be9fd', fg = '#000000', bold = true })
+-- Create autocmd group
+local group = vim.api.nvim_create_augroup('TodoHighlights', { clear = true })
 
--- Apply highlights on buffer events
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'TextChanged', 'InsertLeave' }, {
+-- Apply highlights
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'TextChanged', 'InsertLeave', 'ColorScheme' }, {
+    group = group,
     pattern = '*',
     callback = function()
+        -- Re-define highlights (in case colorscheme changed them)
+        vim.api.nvim_set_hl(0, 'TodoComment', { bg = '#50fa7b', fg = '#000000', bold = true })
+        vim.api.nvim_set_hl(0, 'HackComment', { bg = '#ffb86c', fg = '#000000', bold = true })
+        vim.api.nvim_set_hl(0, 'NoteComment', { bg = '#8be9fd', fg = '#000000', bold = true })
+
         vim.fn.clearmatches()
-        vim.fn.matchadd('TodoComment', '\\c\\<TODO\\>.*$')
-        vim.fn.matchadd('HackComment', '\\c\\<HACK\\>.*$')
-        vim.fn.matchadd('NoteComment', '\\c\\<NOTE\\>.*$')
+        vim.fn.matchadd('TodoComment', '\\c\\<TODO\\>:\\?')
+        vim.fn.matchadd('HackComment', '\\c\\<HACK\\>:\\?')
+        vim.fn.matchadd('NoteComment', '\\c\\<NOTE\\>:\\?')
     end
 })
 
