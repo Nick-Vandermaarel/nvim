1f6423311bc12a18e993204e33120404acadb24f
Nick Vandermaarel <nicholasvdm105@gmail.com>
2025-08-29 12:27:39 -0400

    ts ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -67,8 +67,9 @@ return {
                 end
             })
 
-            local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
-            local vue_language_server_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
+            local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
+                '/vue-language-server' .. '/node_modules/@vue/language-server'
+            local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
 
             local vue_plugin = {
                 name = "@vue/typescript-plugin",
@@ -76,24 +77,20 @@ return {
                 languages = { "vue" },
                 configNamespace = "typescript"
             }
-            local vtsls_config = {
-                settings = {
-                    vtsls = {
-                        tsserver = {
-                            globalPlugins = {
-                                vue_plugin
-                            }
-                        }
-                    }
+            local ts_ls_config = {
+                init_options = {
+                    plugins = {
+                        vue_plugin,
+                    },
                 },
-                filetypes = { "vue", "typescript", "javascript", "javascriptreact", "typescriptreact" }
+                filetypes = tsserver_filetypes,
             }
 
             local vue_ls_config = {}
 
-            vim.lsp.config("vtsls", vtsls_config)
+            vim.lsp.config('ts_ls', ts_ls_config)
             vim.lsp.config("vue_ls", vue_ls_config)
-            vim.lsp.enable({ "vtsls", "vue_ls" })
+            vim.lsp.enable({ "ts_ls", "vue_ls" })
 
             vim.lsp.config("cssls", {
                 settings = {
