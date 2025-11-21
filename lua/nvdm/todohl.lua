149326aadf5bf4423b94b1fb867c95d08fdde455
Nick Vandermaarel <nicholasvdm105@gmail.com>
2025-11-21 10:30:08 -0500

    todo fixes

--- lua/nvdm/todohl.lua
+++ lua/nvdm/todohl.lua
@@ -1,25 +1,22 @@
 -- Custom Highlight TODO, HACK, and NOTE comments
 
 local M = {}
-
 function M.setup()
-    -- Create autocmd group
     local group = vim.api.nvim_create_augroup('TodoHighlights', { clear = true })
 
-    -- Apply highlights
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'TextChanged', 'InsertLeave', 'ColorScheme' }, {
         group = group,
         pattern = '*',
         callback = function()
-            -- Re-define highlights (in case colorscheme changed them)
             vim.api.nvim_set_hl(0, 'TodoComment', { bg = '#50fa7b', fg = '#000000', bold = true })
             vim.api.nvim_set_hl(0, 'HackComment', { bg = '#ffb86c', fg = '#000000', bold = true })
             vim.api.nvim_set_hl(0, 'NoteComment', { bg = '#8be9fd', fg = '#000000', bold = true })
 
             vim.fn.clearmatches()
-            vim.fn.matchadd('TodoComment', '\\c\\<TODO\\>:\\?')
-            vim.fn.matchadd('HackComment', '\\c\\<HACK\\>:\\?')
-            vim.fn.matchadd('NoteComment', '\\c\\<NOTE\\>:\\?')
+            -- Only match after comment characters
+            vim.fn.matchadd('TodoComment', '\\(//\\|#\\|--\\|/\\*\\|<!--\\).*\\zs\\c\\<TODO\\>:\\?')
+            vim.fn.matchadd('HackComment', '\\(//\\|#\\|--\\|/\\*\\|<!--\\).*\\zs\\c\\<HACK\\>:\\?')
+            vim.fn.matchadd('NoteComment', '\\(//\\|#\\|--\\|/\\*\\|<!--\\).*\\zs\\c\\<NOTE\\>:\\?')
         end
     })
 end
