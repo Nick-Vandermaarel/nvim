14431bd9933cb5871586e2351548ff83a2ecd71d
Nick Vandermaarel <nicholasvdm105@gmail.com>
2025-12-05 11:32:46 -0500

    fix autoformat

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -16,6 +16,10 @@ return {
                 lua = { "stylua" },
                 -- cs = { { "csharpier" } },
             },
+            format_on_save = {
+                timeout_ms = 5000,
+                lsp_fallback = true,
+            }
         })
     end,
 }
