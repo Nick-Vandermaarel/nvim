55633e2ab0a127588faaaf96dceb14153cf28919
Nick Vandermaarel <nicholasvdm105@gmail.com>
2026-01-15 15:36:44 -0500

    conflicting keybind

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -19,7 +19,7 @@ return
         -- Recommended/example keymaps.
         vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
             { desc = "Ask opencode" })
-        vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
+        vim.keymap.set({ "n", "x" }, "<C-s>", function() require("opencode").select() end,
             { desc = "Execute opencode action…" })
         vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
 
