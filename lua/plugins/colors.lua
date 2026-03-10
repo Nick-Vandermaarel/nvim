c63e7ba8cca8468cc1ada0fa04a0881950e87bd3
Nick Vandermaarel <nicholasvdm105@gmail.com>
2026-03-10 16:02:57 -0400

    Trying new colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,28 +1,51 @@
+-- return {
+--     "webhooked/kanso.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("kanso").setup({
+--             keywordStyle = { italic = false },
+--             overrides = function(colors)
+--                 return {
+--                     WinSeparator = { fg = colors.palette.zenBg3 },
+--                     -- Right-click menu (Pmenu) - darker version
+--                     Pmenu = { fg = colors.palette.gray2, bg = colors.palette.zenBg1 },
+--                     PmenuSel = { fg = colors.palette.fg, bg = colors.palette.zenBg2, bold = true },
+--                     PmenuSbar = { bg = colors.palette.zenBg0 },
+--                     PmenuThumb = { bg = colors.palette.gray4 },
+--                     -- Private cs fields
+--                     ["@lsp.type.field.cs"] = { fg = colors.palette.fg },
+--                     ["@lsp.type.constant.cs"] = { fg = colors.palette.orange },
+--
+--                     LspReferenceText = { bg = "#2a2e3e" }, -- Subtle blue-gray
+--                     LspReferenceRead = { bg = "#2a2e3e" },
+--                     LspReferenceWrite = { bg = "#3e3449", bold = true, underline = true }, -- Slightly purple
+--                 }
+--             end,
+--         })
+--         vim.cmd("colorscheme kanso-ink")
+--     end,
+-- }
+
 return {
-    "webhooked/kanso.nvim",
-    lazy = false,
-    priority = 1000,
+    "rebelot/kanagawa.nvim",
     config = function()
-        require("kanso").setup({
+        require("kanagawa").setup({
             keywordStyle = { italic = false },
             overrides = function(colors)
                 return {
-                    WinSeparator = { fg = colors.palette.zenBg3 },
                     -- Right-click menu (Pmenu) - darker version
-                    Pmenu = { fg = colors.palette.gray2, bg = colors.palette.zenBg1 },
-                    PmenuSel = { fg = colors.palette.fg, bg = colors.palette.zenBg2, bold = true },
-                    PmenuSbar = { bg = colors.palette.zenBg0 },
-                    PmenuThumb = { bg = colors.palette.gray4 },
+                    Pmenu = { fg = colors.palette.dragonBlue2, bg = colors.palette.dragonBlack1 },
+                    -- PmenuSel = { fg = colors.palette.fg, bg = colors.palette.zenBg2, bold = true },
+                    -- PmenuSbar = { bg = colors.palette.zenBg0 },
+                    -- PmenuThumb = { bg = colors.palette.gray4 },
+                    --
                     -- Private cs fields
                     ["@lsp.type.field.cs"] = { fg = colors.palette.fg },
-                    ["@lsp.type.constant.cs"] = { fg = colors.palette.orange },
-
-                    LspReferenceText = { bg = "#2a2e3e" }, -- Subtle blue-gray
-                    LspReferenceRead = { bg = "#2a2e3e" },
-                    LspReferenceWrite = { bg = "#3e3449", bold = true, underline = true }, -- Slightly purple
+                    ["@lsp.type.constant.cs"] = { fg = colors.palette.dragonOrange2 },
                 }
             end,
         })
-        vim.cmd("colorscheme kanso-ink")
+        vim.cmd("colorscheme kanagawa-dragon")
     end,
 }
