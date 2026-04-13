56cf3e4c42b0a6f43ce94b4bbd23338bc007193a
Nick Vandermaarel <nicholasvdm105@gmail.com>
2026-04-13 18:46:04 -0400

    easy dotnet

--- plugin/easy-dotnet.lua
+++ plugin/easy-dotnet.lua
@@ -1,12 +1,16 @@
-return {
-    "GustavEikaas/easy-dotnet.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", 'folke/snacks.nvim', },
-    ft = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
-    lazy = true,
-    cmd = "Dotnet",
-    config = function()
-        local dotnet = require("easy-dotnet")
-        dotnet.setup({
+local group = vim.api.nvim_create_augroup("LazyLoadEasyDotnet", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
+    callback = function()
+        vim.api.nvim_del_augroup_by_name("LazyLoadEasyDotnet")
+
+        vim.pack.add({
+            "https://github.com/GustavEikaas/easy-dotnet.nvim",
+            "https://github.com/nvim-lua/plenary.nvim"
+        })
+
+        require("easy-dotnet").setup({
             lsp = {
                 enabled = false,
             },
@@ -32,9 +36,5 @@ return {
             },
             picker = "snacks",
         })
-
-        vim.keymap.set("n", "<C-b>", function()
-            dotnet.testrunner_refresh_build()
-        end)
-    end
-}
+    end,
+})
