bd6cbfdfd720d0eb1ec7e497eea00685b5d76826
Nick Vandermaarel <nicholasvdm105@gmail.com>
2026-05-07 11:45:29 -0400

    delete nvim tree

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -114,6 +114,7 @@ vim.pack.add({
 require("snacks").setup({
     input = {},
     notifier = {},
+    explorer = {},
     indent = {
         priority = 1,
         enabled = true,
@@ -203,3 +204,7 @@ end, { desc = "Grep Open Buffers" })
 vim.keymap.set({ "n", "x" }, "<leader>sw", function()
     Snacks.picker.grep_word()
 end, { desc = "Visual selection or word" })
+
+-- Explorer
+vim.keymap.set("n", "<leader>ee", Snacks.explorer.open, { desc = "Toggle explorer" })
+vim.keymap.set("n", "<leader>ef", Snacks.explorer.reveal, { desc = "Toggle file explorer or current file" })
