e5228aa919ebfd3a7c52d9859dfa47572aa719f9
Nick Vandermaarel <nicholasvdm105@gmail.com>
2026-05-20 10:50:22 -0400

    diffview

--- init.lua
+++ init.lua
@@ -36,6 +36,7 @@ vim.pack.add({
     "https://github.com/nvim-treesitter/nvim-treesitter",
 
     -- Less important
+    "https://github.com/sindrets/diffview.nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/romamihalich/neogen",
     "https://github.com/MeanderingProgrammer/render-markdown.nvim",
@@ -50,6 +51,7 @@ require("nvdm.todohl").setup()
 require("lazydev").setup()
 require("nvim-ts-autotag").setup()
 require("mini.pairs").setup()
+require("diffview").setup()
 
 ---
 -- Treesitter
